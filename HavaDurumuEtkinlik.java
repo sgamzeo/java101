988bf402d49ef5a12fcb4f98b45e35dd61d5c4da
Gamze <89346098+sgamzeo@users.noreply.github.com>
2022-09-19 00:35:13 +0300

    Create HavaDurumuEtkinlik.java
    Koşullar :
    
    Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
    Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
    Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
    Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
    Ödev
    Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi farklı çözüm yolları bulunuz.

---
+++ HavaDurumuEtkinlik.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class HavaDurumuEtkinlik {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Hava sıcaklığı: ");
+        heat = inp.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Kayak yapmak için mükemmel bir gün.");
+        } else if (heat >= 5 && heat <= 25) {
+            if (heat <= 15) {
+                System.out.println("Sinema fikri kulağa hoş geliyor.");
+            }
+
+            if (heat >= 15) {
+                System.out.println("Bugün tam piknik havası var.");
+            }
+
+
+        } else {
+            System.out.println("Bugün hava şartları sadece yüzmeye elverişli gibi görünüyor.");
+
+
+        }
+    }
+}
